faa77c7c52e50b4cbced680c8ca80df0ce4dde38
SiumLhahah <114252827+siiuml@users.noreply.github.com>
2025-05-13 10:38:32 +0000

    ByteArray.putNatural(Byte) fixed thus update version to 2.0.1
    Signed-off-by: SiumLhahah <114252827+siiuml@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 }
 
 group = "fyi.ioclub"
-version = "2.0.0-SNAPSHOT"
+version = "2.0.1-SNAPSHOT"
 
 repositories {
     maven { url = uri("https://maven.aliyun.com/repository/public") }
